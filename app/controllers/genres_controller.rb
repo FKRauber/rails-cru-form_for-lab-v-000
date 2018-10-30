7454763a3bd60c955c6f20ee8da2cedb905277c4
Fabienne Rauber <fabienne.rauber@gmail.com>
2018-10-30 02:55:02 +0000

    automatically backed up by learn

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -1,27 +1,27 @@
 class GenresController < ApplicationController
   def index
-    @artists = Artist.all
+    @genres = Genre.all
   end
 
   def show
-    @artist = Artist.find(params[:id])
+    @genre = Genre.find(params[:id])
   end
 
   def new
-    @artist = Artist.new
+    @genre = Genre.new
   end
   def create
-    @artist = Artist.new(params.require(:artist).permit(:name, :bio))
-    @artist.save
-    redirect_to artist_path(@artist)
+    @genre = Genre.new(params.require(:genre).permit(:name, :bio))
+    @genre.save
+    redirect_to genre_path(@genre)
   end
 
   def edit
-    @artist = Artist.find(params[:id])
+    @genre = Genre.find(params[:id])
   end
   def update
-    @artist = Artist.find(params[:id])
-    @artist.update(params.require(:artist).permit(:name, :bio))
-    redirect_to artist_path(@artist)
+    @genre = Genre.find(params[:id])
+    @genre.update(params.require(:genre).permit(:name, :bio))
+    redirect_to genre_path(@genre)
   end
 end
