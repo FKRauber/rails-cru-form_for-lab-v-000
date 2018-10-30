eb42394da25412c43fac0f919af63cf8bc3a1c45
Fabienne Rauber <fabienne.rauber@gmail.com>
2018-10-30 02:49:14 +0000

    automatically backed up by learn

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -8,16 +8,17 @@ class ArtistsController < ApplicationController
   end
 
   def new
-    
+
   end
   def create
-    
+
   end
 
   def edit
-    
+    @artist = Artist.find(params[:id])
+
   end
   def update
-    
+
   end
 end
