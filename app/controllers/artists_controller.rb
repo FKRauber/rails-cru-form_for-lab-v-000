17cdd1e78a486467bf8b9ec01b9f68c3c21f5e87
Fabienne Rauber <fabienne.rauber@gmail.com>
2018-10-30 02:52:36 +0000

    automatically backed up by learn

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -22,5 +22,6 @@ class ArtistsController < ApplicationController
   def update
     @artist = Artist.find(params[:id])
     @artist.update(params.require(:artist).permit(:name, :bio))
+    redirect_to artist_path(@artist)
   end
 end
