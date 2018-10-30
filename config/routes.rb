a4d4ae59c8b5359d5f249b627b6e5dc2b3f65608
Fabienne Rauber <fabienne.rauber@gmail.com>
2018-10-30 02:40:26 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  resources :songs
   resources :genres
   resources :artists
+  resources :songs, only: [:index, :show, :new, :create, :edit, :update]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
